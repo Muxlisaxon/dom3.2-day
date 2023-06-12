be9cfbec88777b7659a2ec7aeb9db00ec1559b2e
Muxlisaxon <qochqorovamuxlisam@gmail.com>
2023-06-12 19:01:25 +0500

    voice

---
+++ index.js
@@ -0,0 +1,42 @@
+let btn1 = document.querySelector(".btn1")
+let btn2 = document.querySelector(".btn2")
+let btn3 = document.querySelector(".btn3")
+let btn4 = document.querySelector(".btn4")
+let btn5 = document.querySelector(".btn5")
+let btn6 = document.querySelector(".btn6")
+
+
+
+let audio1 =document.querySelector(".audio1")
+let audio2 =document.querySelector(".audio2")
+let audio3 =document.querySelector(".audio3")
+let audio4 =document.querySelector(".audio4")
+let audio5 =document.querySelector(".audio5")
+let audio6 =document.querySelector(".audio6")
+
+
+
+btn1.onclick = () => {
+    audio1.play()
+}
+btn2.onclick = () => {
+    audio2.play()
+}
+btn3.onclick = () => {
+    audio3.play()
+}
+
+btn6.onclick = () => {
+    audio6.play()
+}
+
+btn5.onclick = () => {
+    audio1.play()
+}
+btn4.onclick = () => {
+    audio4.play()
+}
+
+
+
+
